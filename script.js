3c01ff5e566ed36a93d0732528150fff91e8d4ad
yungestAmine <purpleamine@gmail.com>
2024-02-18 19:11:33 +0100

    finito grafica

---
+++ script.js
@@ -0,0 +1,163 @@
+class loginForm extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+    connectedCallback(){
+        const path = this.getAttribute("source")
+        let div = document.createElement('div')
+        div.classList.add('container')
+        let imgCont = document.createElement('div')
+        imgCont.classList.add('imgCont')
+        let img = document.createElement('img')
+        img.src = path
+        imgCont.appendChild(img)
+        div.appendChild(imgCont)
+
+        let controlsCont = document.createElement('div')
+        controlsCont.classList.add('ctrl')
+        let a = 
+        `
+            <input type="text" placeholder="Numero di telefono, nome utente o e-mail">
+            <input type="password" placeholder="Password">
+            <button> Accedi</button>
+            <article id="art">
+                <hr size="1">
+                <p>O</p>
+            </article>
+            <div id="fb"><img src="IMG/fb.png"><p> <b>Accedi con Facebook </b></p></div>
+            <a href="">Password dimenticata?</a>
+            <p id="sega">Puoi anche segnalare i contenuti che ritieni violino nel tuo Paese senza accedere.</p>
+        `
+        controlsCont.innerHTML = a
+        div.appendChild(controlsCont)
+        const css = document.createElement('style')
+        css.textContent = 
+        `
+        @import url('https://fonts.googleapis.com/css2?family=Handjet&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:wght@100&family=Nunito:wght@300;400&family=Open+Sans&family=Roboto:wght@100;300&display=swap');
+            *{
+                font-family: "Lato", sans-serif;
+            }
+            .container{
+                width: 100%;
+                height: 100%;
+                border: 0.1px solid rgb(212, 212, 212);
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-content: center;
+            }
+            .ctrl{
+                display:flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+            }
+            .imgCont{
+                display:flex;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+            }
+            .imgCont>img{
+                width: 180px;
+                padding: 50px 50px 30px 50px;
+            }
+            input{
+                width: 72%;
+                margin: 3px;
+                height: 30px;
+                font-size: .75rem;
+                border: 0.1px solid #dadada;
+                background: #f9f9f9;
+                color: #727272;
+                padding-left: 4px;
+                border-radius: 2px;
+            }
+            ::placeholder {
+                color: #727272;
+                opacity: 1;
+            }
+            button{
+                width:74%;
+                margin: 10px 0px 0px 0;
+                height: 30px;
+                border-radius: 5px;
+                border-color: #4eb5f7;
+                background: #4eb5f7;
+                color: #fff;
+                
+            }
+            hr{
+                color: rgb(212,212,212)
+            }
+            #art{
+                width:74%;
+                margin-top: 20px;
+                margin-bottom: 20px;
+                position: relative
+            }
+            #art>p{
+                z-index: 2;
+                width: 40px;
+                height: 15px;
+                background: #fff;
+                position: absolute;
+                text-align: center;
+                top: -90%;
+                left: 40%;
+                font-weight: 50;
+            }
+            #fb{
+                width: 100%;
+                height: 50px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: #405c93;
+            }
+            #fb>img{
+                height: 15px;
+                margin-right: 5px;
+            }
+            a{
+                text-decoration: none;
+                font-size: 0.8rem;
+                color: #03377d;
+            }
+            #sega{
+                text-decoration: none;
+                font-size: 0.8rem;
+                color: #727280;
+                width: 80%;
+                text-align: center;
+            }
+        `
+        this.shadowRoot.appendChild(div)
+        this.shadowRoot.appendChild(css)
+
+    }
+}
+customElements.define("marco-abalintoaie", loginForm)
+const array = [
+    "Meta",
+    "Informazioni",
+    "Blog",
+    "Lavora con noi",
+    "Aiuto",
+    "API",
+    "Privacy",
+    "Condizioni",
+    "Luoghi",
+    "Instagram Lite",
+    "Threads",
+    "Caricamento dei contatti e non-utenti",
+    "Meta Verified"
+];
+let footer = document.querySelector('footer')
+array.forEach(item => {
+    let a = document.createElement('span')
+    a.textContent = item
+    footer.appendChild(a);
+})
